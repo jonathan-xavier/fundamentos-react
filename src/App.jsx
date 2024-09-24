a34b0dc4c668b88fe84deb7a122e270f29646efb
Jonathan Xavier <jonathan.rodrigues@mv.com.br>
2024-09-24 14:19:47 -0300

    feature: Adicionando sidebar e confingurando estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar";
+
 export function App() {
   return (
     <div>
 
-      <Header/>
-      <Post
-        author="Jonathan"
-        content="ababaweweowiek"      
-      />
+      <Header />
+
+
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="Jonathan"
+            content="ababaweweowiek"
+          />
+          <Post
+            author="Jonathan"
+            content="ababaweweowiek"
+          />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,8 @@
+import styles from './Sidebar.module.css'
+export function Sidebar(){
+    return (
+        <aside className={styles.sidebar}>
+            <img src="" alt="" />
+        </aside>
+    );
+}
